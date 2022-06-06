d4e2129d54fd73c4a6a7a409ce4894f367318541
Linhx <phantranthelinh@gmail.com>
2022-06-06 22:11:41 +0700

    up

--- src/Routes/departmentRoute.js
+++ src/Routes/departmentRoute.js
@@ -3,10 +3,10 @@ const departmentController = require('../Controller/departmentController')
 const router = express.Router()
 const { admin, protect } = require('../middleware/Auth')
 
+router.get('/', protect, admin, departmentController.getAll)
 router.delete('/:id', protect, admin, departmentController.delete)
 router.get('/:id', protect, admin, departmentController.get)
 router.put('/:id', protect, admin, departmentController.update)
 router.post('/', protect, admin, departmentController.add)
-router.get('/', protect, admin, departmentController.getAll)
 
 module.exports = router
