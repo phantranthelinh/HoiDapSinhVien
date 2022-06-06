e127a8b5f441519d118feca2c693f21f23c02949
Linhx <phantranthelinh@gmail.com>
2022-06-06 21:02:34 +0700

    up

--- src/Controller/departmentController.js
+++ src/Controller/departmentController.js
@@ -4,8 +4,8 @@ const postionController = {
   add: asyncHandler(async (req, res) => {
     try {
       const { name } = req.body
-      const postionExits = await Department.findOne({ name })
-      if (postionExits) {
+      const nameExits = await Department.findOne({ name })
+      if (nameExits) {
         res.status(400).json('Bộ phận đã tồn tại')
       }
       const position = await Department.create({
@@ -13,6 +13,7 @@ const postionController = {
       })
       res.status(200).json('Thêm mới bộ phận thành công')
     } catch (err) {
+      res.status(400).json(err)
       throw new Error('Thêm mới bộ phận thất bại!!!')
     }
   }),
