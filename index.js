80234b1e9022d8c5111177ea61d1fc4179f344ec
Linhx <phantranthelinh@gmail.com>
2022-07-04 14:10:44 +0700

    up

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-
-app.use(express.static(path.join(__dirname, 'uploads')))
+app.use('/uploads', express.static('uploads'))
+app.use(express.static(__dirname))
 
 //ROUTES
 
--- src/Routes/QnARoute.js
+++ src/Routes/QnARoute.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const multer = require('multer')
-
+const path = require('path')
 const fileStorageEngine = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, './uploads')
