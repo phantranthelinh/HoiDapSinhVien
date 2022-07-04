0fcaf652f16f23b317a0e78859eff3574be2ea46
Linhx <phantranthelinh@gmail.com>
2022-07-04 16:24:15 +0700

    up

--- src/Routes/QnARoute.js
+++ src/Routes/QnARoute.js
@@ -2,7 +2,7 @@ const express = require('express')
 const multer = require('multer')
 const fileStorageEngine = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'uploads')
+    cb(null, 'public/export')
   },
   filename: (req, file, cb) => {
     cb(null, file.originalname)
