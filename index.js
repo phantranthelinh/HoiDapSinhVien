f26e8e133feaf01adf32dfde10cab8087ab765fb
Linhx <phantranthelinh@gmail.com>
2022-07-04 16:29:26 +0700

    up

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use('/', express.static('public'))
+app.use('/', express.static(path.join(__dirname, 'public')))
 
 //ROUTES
 
